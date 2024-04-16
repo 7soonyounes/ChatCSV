a5a3583455df71e34a222edbce152d15a21ee56e
7soonyounes <nonshsoon@gmail.com>
2024-04-16 11:39:21 +0100

    worked !

--- main.py
+++ main.py
@@ -1,54 +1,65 @@
 from dotenv import find_dotenv, load_dotenv
-from langchain_openai import OpenAI, ChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain.chat_models import AzureChatOpenAI
-from langchain.agents.agent_types import AgentType
-
 import pandas as pd
 import os
 import streamlit as st
 import matplotlib.pyplot as plt
-import seaborn as sns
-import json
+import io
+import contextlib
 
 def main():
+    def extract_code(response):
+                if '```python' in response:
+                    start = response.find('```python') + len('```python')
+                    end = response.find('```', start)
+                    return response[start:end].strip()
+                return None
+
+    def execute_code(code, df):
+        local_vars = {'df': df, 'plt': plt, 'pd': pd}
+        buffer = io.StringIO()
+        with contextlib.redirect_stdout(buffer):
+            exec(code, globals(), local_vars)
+        output = buffer.getvalue()
+        return output.strip()
+
     load_dotenv(find_dotenv())
 
-    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-        print("OPENAI_API_KEY is not set")
-        exit(1)
-    else:
-        print("OPENAI_API_KEY is set")
+    if not os.getenv("AZURE_OPENAI_API_KEY"):
+        st.error("OPENAI_API_KEY is not set")
+        st.stop()
 
     st.set_page_config(page_title="Ask Ask")
-    st.header("Ask 📈")
+    st.header("Ask CSV 📈")
 
     file = st.file_uploader("Upload a CSV or excel file", type=["csv"])
+    
     if file is not None:
-        df = pd.read_csv(file)
+            
+            st.info("CSV Uploaded Successfully")
+            df = pd.read_csv(file)
+            st.dataframe(df, use_container_width=True)
+            
+            llm = AzureChatOpenAI(temperature=0, model_name='gpt-3.5-turbo', api_version="2023-03-15-preview", azure_endpoint="https://imagepdfprocessing.openai.azure.com/", azure_deployment="chat-endpoint")
+            agent = create_pandas_dataframe_agent(llm, df, agent_type="openai-tools", verbose=True)
 
-        llm = ChatOpenAI(temperature=0, model="gpt-4-1106-preview")
-        agent = create_pandas_dataframe_agent(llm, df, agent_type="openai-tools", verbose=True)
+            user_question = st.text_input("Ask a question about your CSV: ")
+            if user_question:
+                instruction = "Please say 'Hello!' before reply to the query. You must need to use matplotlib library to create any graph. Here is the query: "
+                prompt = instruction + user_question
+                with st.spinner(text="In progress..."):
+                    response = agent.invoke(prompt, handle_parsing_errors=True)
+                    code = extract_code(response.get("output", ""))  
 
-        
-        # llm = AzureChatOpenAI(
-        #     temperature=0,
-        #     deployment_name="chat-endpoint",
-        #     model_name="gpt-35-turbo"
-        # )
-        # agent = agent = create_pandas_dataframe_agent(
-        #             llm,
-        #             df,
-        #             verbose=True,
-        #             agent_type=AgentType.OPENAI_FUNCTIONS,
-        #         )
-
-        user_question = st.text_input("Ask a question about your CSV: ")
-
-        if user_question is not None and user_question != "":
-            with st.spinner(text="In progress..."):
-                resp = agent.invoke(user_question)["output"]
-                st.write(resp)
+                    if code:
+                        result = execute_code(code, df)
+                        if 'plt.show' in code:
+                            st.pyplot(plt)
+                        elif result is not None:
+                            st.write(result)
+                    else:
+                        st.write("Received non-executable response: ", response.get("output", "No response or incorrect format received."))
 
 if __name__ == "__main__":
     main()
